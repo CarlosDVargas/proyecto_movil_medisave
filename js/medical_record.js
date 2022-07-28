220f1ce5c7e9c6a288d6595858684132c6f34b7c
Carlos Daniel Vargas Varela <carlos.vargas@ucrso.info>
2022-07-28 00:05:09 -0600

    Se agrega módulo para la descarga de Examenes médicos

--- js/medical_record.js
+++ js/medical_record.js
@@ -14,12 +14,14 @@ $(document).ready(function () {
       var treatments = medicalRecord.treatments;
       var medicines = medicalRecord.medicines;
       var notes = medicalRecord.medicalHistories;
+      var tests = medicalRecord.tests;
       $('#patientName').text(user.dni + " - " + user.name)
       console.log(medicalRecord);
       showTreatment(treatments)
       showMedicines(medicines)
       showIllnesess(illnesses)
       showMedicalHistory(notes)
+      showMedicalTests(tests)
     }
   });
 
@@ -131,5 +133,53 @@ function showMedicalHistory(historyNotes) {
       </div>`;
     i += 1;
     $(".medicalHistory").append(text);
+  });
+}
+
+function showMedicalTests(tests) {
+  var i = 1;
+  tests.forEach(test => {
+    var doctorName = test.doctorName;
+    var creationDate = new Date(test.creationDate);
+    var desc = test.description;
+    var server = "https://localhost:44392/"
+    var downloadUrl = test.fileUrl;
+    var text =
+      `<div class="container p-2">
+        <div class="card">
+          <div class="row text-center">
+            <div class="col-md-12 mt-2">
+              <h6 class="mb-0">Medical Tests ${i}</h6>
+              <hr/>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12 m-2">
+              <h6 class="mb-0">Doctor: ${doctorName}</h6>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12 m-2">
+              <h6 class="mb-0">Date: ${creationDate}</h6>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <div class="card-body">
+                <p>${desc}</p>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12 m-2">
+              <div class="card-body">
+                < a href = "${server + downloadUrl}"
+                download type = "application/octet-stream" > Download test < /a>
+              </div>
+          </div>
+        </div>
+      </div>`;
+    i += 1;
+    $(".medicalTests").append(text);
   });
 }
