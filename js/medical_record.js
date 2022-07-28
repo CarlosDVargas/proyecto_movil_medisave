29b67b2a005e9362599c9fc752b294cacfaf0b45
MAURICIO.ROJASSALAS <MAURICIO.ROJASSALAS@ucr.ac.cr>
2022-07-27 22:34:04 -0600

    Segundo commit

--- js/medical_record.js
+++ js/medical_record.js
@@ -11,15 +11,28 @@ $(document).ready(function () {
       medicalRecord = info.data;
       user = medicalRecord.applicationUser;
       var illnesses = medicalRecord.illnesses;
+      var treatments = medicalRecord.treatments;
+      var medicines = medicalRecord.medicines;
       $('#patientName').text(user.dni + " - " + user.name)
       console.log(medicalRecord);
-      illnesses.forEach(illness => {
-        var illnessName = illness.name;
-        var illnessDescription = illness.description;
-        $("#example").append(illnessName, illnessDescription);
-      });
+      showTreatment(treatments)
+      showMedicines(medicines)
     }
   });
 
 });
 
+function showTreatment(treatments) {
+  treatments.forEach(treatments => {
+    var name = treatments.name;
+    var desc = treatments.description;
+    $(".treatments").append(name, desc);
+  });
+}
+
+function showMedicines(medicines) {
+  medicines.forEach(medicines => {
+    var name = medicines.name;
+    $(".medicines").append(name);
+  });
+}
