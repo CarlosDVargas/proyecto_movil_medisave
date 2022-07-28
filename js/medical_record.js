a05b6bbd1cbdd9ce8efd163079c9c3b0bf309ae2
Carlos Daniel Vargas Varela <carlos.vargas@ucrso.info>
2022-07-27 23:18:54 -0600

    Se agrega modulo de muestra de las enfermedades y el historial

--- js/medical_record.js
+++ js/medical_record.js
@@ -13,10 +13,13 @@ $(document).ready(function () {
       var illnesses = medicalRecord.illnesses;
       var treatments = medicalRecord.treatments;
       var medicines = medicalRecord.medicines;
+      var notes = medicalRecord.medicalHistories;
       $('#patientName').text(user.dni + " - " + user.name)
       console.log(medicalRecord);
       showTreatment(treatments)
       showMedicines(medicines)
+      showIllnesess(illnesses)
+      showMedicalHistory(notes)
     }
   });
 
@@ -34,5 +37,70 @@ function showMedicines(medicines) {
   medicines.forEach(medicines => {
     var name = medicines.name;
     $(".medicines").append(name);
+  });
+}
+
+function showIllnesess(illnesses) {
+  illnesses.forEach(illness => {
+    var name = illness.name;
+    var desc = illness.description;
+    var text =
+      `<div class="container p-2">
+        <div class="card">
+          <div class="row text-center">
+            <div class="col-md-12 mt-2">
+              <h6 class="mb-0">${name}</h6>
+              <hr/>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <div class="card-body">
+                <p>${desc}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`;
+    $(".illnesses").append(text);
+  });
+}
+
+function showMedicalHistory(historyNotes) {
+  var i = 1;
+  historyNotes.forEach(notes => {
+    var doctorName = notes.doctorName;
+    var creationDate = new Date(notes.creationDate);
+    var desc = notes.description;
+    var text =
+      `<div class="container p-2">
+        <div class="card">
+          <div class="row text-center">
+            <div class="col-md-12 mt-2">
+              <h6 class="mb-0">Medical History Entry ${i}</h6>
+              <hr/>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12 m-2">
+              <h6 class="mb-0">Doctor: ${doctorName}</h6>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12 m-2">
+              <h6 class="mb-0">Date: ${creationDate}</h6>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <div class="card-body">
+                <p>${desc}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`;
+    i += 1;
+    $(".medicalHistory").append(text);
   });
 }
